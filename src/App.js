88ccdfb9e27e50fc172a48a7b6e130641bc87b94
Hasham <hasham.hasham1@gmail.com>
2020-04-12 13:55:20 +0500

    list and conditions

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import Person from "./Person/PersonComponent";
 class App extends Component {
   state = {
     person: [
-      { name: "Hasham", age: 22 },
-      { name: "Saif", age: 20 },
-      { name: "Aqsa", age: 16 },
+      { id: 1, name: "Hasham", age: 22 },
+      { id: 2, name: "Saif", age: 20 },
+      { id: 3, name: "Aqsa", age: 16 },
     ],
   };
+
   switchNameHandler = (newName) => {
     this.setState({
       person: [
@@ -16,15 +17,31 @@ class App extends Component {
         { name: "Saif javed", age: 22 },
         { name: "Aqsa javed", age: 18 },
       ],
+      toggleValue: false,
     });
   };
-  changeHandler = (event) => {
+  changeHandler = (event, id) => {
+    const personIndex = this.state.person.findIndex((p) => p.id == id);
+    const person = {
+      ...this.state.person[personIndex],
+    };
+    person.name = event.target.value;
+    const persons = [...this.state.person];
+    persons[personIndex] = person;
     this.setState({
-      person: [
-        { name: "Hasham", age: 25 },
-        { name: event.target.value, age: 22 },
-        { name: "Aqsa javed", age: 18 },
-      ],
+      person: persons,
+    });
+  };
+  deletePersonHandler = (personIndex) => {
+    //const personArr = this.state.person; // this can mutate the orignal array becasue this is reference
+    const personArr = [...this.state.person]; // this is the better approch u cna also use slice() method
+    personArr.splice(personIndex, 1);
+    this.setState({ person: personArr });
+  };
+  toggleHandler = () => {
+    const temp = this.state.toggleValue;
+    this.setState({
+      toggleValue: !temp,
     });
   };
   render() {
@@ -35,32 +52,33 @@ class App extends Component {
       padding: "8px",
       cursor: "pointer",
     };
+    let persons = null;
+    if (this.state.toggleValue) {
+      persons = (
+        <div>
+          {this.state.person.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                changed={(event) => this.changeHandler(event, person.id)}
+                key={person.id}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi i am react App</h1>
         <p>I am a working</p>
-        <button
-          style={style}
-          onClick={() => this.switchNameHandler("Hasham!!!!")}
-        >
-          Switch name
+        <button style={style} onClick={this.toggleHandler}>
+          Toggle Name
         </button>
-        <Person
-          name={this.state.person[0].name}
-          age={this.state.person[0].age}
-        />
-        <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
-          click={this.switchNameHandler.bind(this, "Bisma")}
-          changed={this.changeHandler}
-        >
-          My hobby is : Mobile using
-        </Person>
-        <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age}
-        />
+        {persons}
       </div>
     );
   }
